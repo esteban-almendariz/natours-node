8d131297317737ff8b7e4a048e2456ecbd439e70
esteban-almendariz <98506796+esteban-almendariz@users.noreply.github.com>
2022-10-21 15:31:18 -0500

    structured functions to new files and added param middleware

--- app.js
+++ app.js
@@ -1,73 +1,34 @@
 const express = require('express');
-const fs = require('fs')
+
+const morgan = require('morgan')
+
+const tourRouter = require('./routes/tourRoutes')
+const userRouter = require('./routes/userRoutes')
 
 const app = express();
 
+app.use(morgan('dev'))
 app.use(express.json())
 
-// app.get('/', (req, res) => {
-//     res.status(200).json({ message: 'Hello from the server side!', app: 'Natours' })
-// })
-
-// app.post('/', (req, res) => {
-//     res.send('You can send to this post method')
-// })
-
-
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
-
-app.get('/api/v1/tours', (req, res) => {
-    res.json({
-        status: 'success',
-        results: tours.length,
-        data: {
-            tours: tours
-        }
-
-    })
+app.use((req, res, next) => {
+    console.log('Hello from the middlware')
+    next();
 });
 
-app.get('/api/v1/tours/:id', (req, res) => {
-    console.log(req.params)
-
-    const id = req.params.id * 1;
-    const tour = tours.find(el => el.id === req.params)
-
-    if (id > tours.length) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        })
-    }
-
-    res.json({
-        status: 'success',
-        data: {
-            tours: tours[id]
-        }
-    })
+app.use((req, res, next) => {
+    req.requestTime = new Date().toISOString();
+    next();
 })
 
-app.post('/api/v1/tours', (req, res) => {
-    // console.log(req.body)
-    const newId = tours[tours.length - 1].id + 1;
-    const newTour = Object.assign({ id: newId }, req.body)
 
-    tours.push(newTour)
+// Routes
 
-    fs.writeFileSync(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-        res.status(201).json({
-            status: 'success',
-            data: {
-                tours: newTour
-            }
-        })
-    })
 
-})
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter)
 
-const port = 3000;
-app.listen(port, () => {
-    console.log('App running on port 3000')
-})
+
+module.exports = app;
+
+
 
